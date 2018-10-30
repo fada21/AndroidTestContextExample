10687d7ac5aafc5675ff08392de4a7e303fdad3c
Łukasz Czaplicki <lukasz.czaplicki@gmail.com>
2018-10-30 11:24:18 +0000

    AndroidTestContextExample code

---
+++ app/src/androidTest/java/playground/fada21/com/mytestapplication/ExampleInstrumentedTest.kt
@@ -0,0 +1,40 @@
+package playground.fada21.com.mytestapplication
+
+import android.app.Application
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    fun useInstrumentationContext() {
+        val appContext = InstrumentationRegistry.getInstrumentation().context
+        assertThat("playground.fada21.com.mytestapplication.test").isEqualTo(appContext.packageName)
+    }
+
+    @Test
+    fun useAppContextOldschool() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertThat("playground.fada21.com.mytestapplication").isEqualTo(appContext.packageName)
+        assertEquals("playground.fada21.com.mytestapplication", appContext.packageName)
+    }
+
+    @Test
+    fun useAppContextNewRecommendedWay() {
+        val app: Context = ApplicationProvider.getApplicationContext() // Context here might be YourAppClass
+        assertEquals("playground.fada21.com.mytestapplication", app.packageName)
+    }
+
+}
